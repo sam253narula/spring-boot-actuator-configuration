10d4b6f621579df9caf92e258aeb5044734eb7b6
sam253narula <samarthnarula253@gmail.com>
2019-06-02 17:56:32 +0530

    first

---
+++ src/main/java/com/spring/boot/health/actuatorexample/ActuatorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.health.actuatorexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorExampleApplication.class, args);
+	}
+	
+}
